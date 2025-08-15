07589da40b0c51f0aabdb94c97b592bb971a88fd
Harirajhari <hariraj121203@gmail.com>
2025-08-15 22:44:43 +0530

    changed the procy server

--- server.js
+++ server.js
@@ -37,6 +37,26 @@ app.use(express.json());
 app.use('/api/admins', adminRoutes);
 app.use('/api/pages', pageRoutes);
 
+app.get('/proxy-image', async (req, res) => {
+    try {
+        const imageUrl = req.query.url; // Get the image URL from the query parameter
+        if (!imageUrl) {
+            return res.status(400).send('Image URL is required.');
+        }
+
+        const response = await fetch(imageUrl);
+        const buffer = await response.buffer();
+        const contentType = response.headers.get('content-type');
+
+        res.header('Access-Control-Allow-Origin', '*'); // Set CORS header
+        res.header('Content-Type', contentType);
+        res.send(buffer);
+    } catch (error) {
+        console.error('Error proxying image:', error);
+        res.status(500).send('Error loading image.');
+    }
+});
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
